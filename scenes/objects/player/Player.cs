561bbd68a16980120608268d2329b58916149d2e
sirsegv <sirsegv@sirsegv.moe>
2024-11-05 10:43:21 +1030

    add basic movement

--- scenes/objects/player/Player.cs
+++ scenes/objects/player/Player.cs
@@ -3,4 +3,14 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	[Export] float Speed = 10;
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		var inputVec = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+
+		Velocity = inputVec * Speed;
+
+		MoveAndSlide();
+	}
 }
