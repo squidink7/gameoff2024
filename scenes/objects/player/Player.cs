e5fe8b6692434605c938efd1e1bc60e9b8c639e0
sirsegv <sirsegv@sirsegv.moe>
2024-11-07 12:15:37 +1030

    properly implement lightmasking

--- scenes/objects/player/Player.cs
+++ scenes/objects/player/Player.cs
@@ -4,10 +4,16 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	[Export] float Speed = 10;
+	[Export] Node3D Model;
 	
 	public override void _PhysicsProcess(double delta)
 	{
 		var inputVec = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+
+		if (inputVec != Vector2.Zero)
+		{
+			Model.Rotation = Vector3.Up * (-inputVec.Angle() + Mathf.Pi/4);
+		}
 
 		Velocity = inputVec * Speed;
 
