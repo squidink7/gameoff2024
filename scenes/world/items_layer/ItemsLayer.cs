5116d299467afcce5ead3f5ccd297d423ec002f5
sirsegv <sirsegv@sirsegv.moe>
2024-11-13 00:02:54 +1030

    even betterer lightmasking

--- scenes/world/items_layer/ItemsLayer.cs
+++ scenes/world/items_layer/ItemsLayer.cs
@@ -3,32 +3,34 @@ using System;
 
 public partial class ItemsLayer : CanvasGroup
 {
+	public static Node LightmaskRoot;
+
 	Camera2D Camera;
 
-	// Locate and set shader material
 	public override void _Ready()
 	{
-		// return;
+		// Root node for all lightmasks to parent themselves to
+		LightmaskRoot = GetNode("LightmaskViewport");
+		
+		// Find Player in scene and get reference to their camera
 		var players = GetParent().FindChildren("Player", "");
 
 		if (players.Count == 0)
 		{
 			GD.Print("No Player in scene, All Items visible");
+			Material = null;
 			return;
 		}
 
 		var player = players[0] as Player;
 		
 		Camera = player.GetNode<Camera2D>("WorldCamera");
-
-		// Render everything in the game world
-		GetNode<SubViewport>("LightmaskViewport").World2D = GetViewport().World2D;
-		// Set main viewport to not show lightmasks
-		GetTree().Root.GetViewport().SetCanvasCullMaskBit(1, false);
 	}
 
 	public override void _Process(double delta)
 	{
+		// Follow Player's camera
+		if (Camera == null) return;
 		GetNode<Camera2D>("LightmaskViewport/Camera").GlobalPosition = Camera.GlobalPosition;
 	}
 }
